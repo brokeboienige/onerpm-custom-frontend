621b370b4fee0c61f851b81ee2267334d0932e7b
BuildTools <enige@protonmail.com>
2022-08-20 12:57:05 -0300

    base

---
+++ onerpm-frontend.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         ONErpm custom frontend
+// @namespace    http://github.com/brokeboienige/onerpm-custom-frontend
+// @version      0.1
+// @description  A custom, non-official frontend for the ONErpm music distributor.
+// @author       You
+// @match        https://dashboard.onerpm.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=onerpm.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert(1)
+})();
